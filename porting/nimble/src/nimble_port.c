29722059a54203a8697cc519a6cf50cfaa300b03
Sagar Bijwe <sagar@espressif.com>
2019-01-02 15:03:04 +0530

    esp-nimble: Add support for logging
    This change adds support for nimble logging using IDF APIs. By default,
    nimble logging level is set to follow global logging configuration.

--- porting/nimble/src/nimble_port.c
+++ porting/nimble/src/nimble_port.c
@@ -26,6 +26,9 @@
 #include "controller/ble_ll.h"
 #include "nimble/transport.h"
 #endif
+#ifdef ESP_PLATFORM
+#include "esp_log.h"
+#endif
 
 static struct ble_npl_eventq g_eventq_dflt;
 
@@ -52,6 +55,11 @@ nimble_port_init(void)
 #endif
     ble_transport_ll_init();
 #endif
+
+#ifdef ESP_PLATFORM
+    esp_log_level_set("NimBLE", LOG_LOCAL_LEVEL);
+#endif
+
 }
 
 void
