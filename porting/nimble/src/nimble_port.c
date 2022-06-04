f547144fe4ed5a57636c4dc0d55b1bdf2df06f76
Geng Yuchao <gengyuchao@espressif.com>
2022-06-04 11:33:56 +0800

    Fix macro issue for c2 controller

--- porting/nimble/src/nimble_port.c
+++ porting/nimble/src/nimble_port.c
@@ -123,7 +123,7 @@ nimble_port_deinit(void)
 #endif
 }
 
-
+#if CONFIG_BT_NIMBLE_ENABLED
 /**
  * Called when the host stop procedure has completed.
  */
@@ -132,6 +132,7 @@ ble_hs_stop_cb(int status, void *arg)
 {
     ble_npl_sem_release(&ble_hs_stop_sem);
 }
+#endif
 
 static void
 nimble_port_stop_cb(struct ble_npl_event *ev)
