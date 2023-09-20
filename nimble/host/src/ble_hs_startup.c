12df982603c69e0df85d2619f6ae2f248ae52a4c
Rahul Tank <rahul.tank@espressif.com>
2023-09-20 12:49:25 +0530

    Added change to generate unique IRK for each chip

--- nimble/host/src/ble_hs_startup.c
+++ nimble/host/src/ble_hs_startup.c
@@ -349,8 +349,9 @@ ble_hs_startup_reset_tx(void)
 int
 ble_hs_startup_go(void)
 {
-    struct ble_store_gen_key gen_key;
+    //struct ble_store_gen_key gen_key;
     int rc;
+    uint8_t irk[16];
 
     rc = ble_hs_startup_reset_tx();
     if (rc != 0) {
@@ -403,7 +404,7 @@ ble_hs_startup_go(void)
     if (rc != 0) {
         return rc;
     }
-
+#if 0
     if (ble_hs_cfg.store_gen_key_cb) {
         memset(&gen_key, 0, sizeof(gen_key));
         rc = ble_hs_cfg.store_gen_key_cb(BLE_STORE_GEN_KEY_IRK, &gen_key,
@@ -418,6 +419,10 @@ ble_hs_startup_go(void)
     if (rc != 0) {
         ble_hs_pvcy_set_our_irk(NULL);
     }
+#endif
+    ble_hs_pvcy_set_default_irk();
+
+    ble_hs_pvcy_set_our_irk(irk);
 
     /* If flow control is enabled, configure the controller to use it. */
     ble_hs_flow_startup();
