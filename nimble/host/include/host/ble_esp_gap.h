a2164b808faeeb01aab4c80839a6ff8ce3f10609
Rahul Tank <rahul.tank@espressif.com>
2024-01-17 14:24:59 +0530

    Added API to get current stack operation status

--- nimble/host/include/host/ble_esp_gap.h
+++ nimble/host/include/host/ble_esp_gap.h
@@ -11,6 +11,26 @@
 extern "C" {
 #endif
 
+enum gap_status {
+    BLE_GAP_STATUS_ADV = 0,
+    BLE_GAP_STATUS_EXT_ADV,
+    BLE_GAP_STATUS_SCAN,
+    BLE_GAP_STATUS_CONN,
+    BLE_GAP_STATUS_PAIRED,
+    BLE_GAP_STATUS_GATTS,
+    BLE_GAP_STATUS_HOST_PRIVACY,
+    BLE_GAP_STATUS_PERIODIC,
+};
+
+typedef enum gap_status gap_status_t;
+
+/**
+ * This API gives the current status of various stack operations
+ *
+ * @return                      0 on success; nonzero bits indicating different
+ *                              operations as per enum gap_status.
+ */
+int ble_gap_host_check_status(void);
 
 #if MYNEWT_VAL(BLE_HCI_VS)
 /**
