2107460209742b5e99d2bb126678e58ac4823758
Rahul Tank <rahul.tank@espressif.com>
2024-03-22 10:54:14 +0530

    Add support to handle Data Length change event

--- nimble/host/include/host/ble_gap.h
+++ nimble/host/include/host/ble_gap.h
@@ -160,7 +160,7 @@ struct hci_conn_update;
 #define BLE_GAP_EVENT_REATTEMPT_COUNT       31
 #define BLE_GAP_EVENT_AUTHORIZE             32
 #define BLE_GAP_EVENT_TEST_UPDATE           33
-
+#define BLE_GAP_EVENT_DATA_LEN_CHG          34
 
 /* DTM events */
 #define BLE_GAP_DTM_TX_START_EVT            0
@@ -1246,6 +1246,29 @@ struct ble_gap_event {
 	     */
             uint16_t num_pkt;
 	} dtm_state;
+
+        /**
+	 * Represent an event for LE Data length change
+	 *
+	 * Valid for the following event types:
+	 *      o BLE_GAP_EVENT_DATA_LEN_CHG
+	 */
+	struct {
+            /* Connection handle */
+	    uint16_t conn_handle;
+
+	    /* Max Tx Payload octotes */
+	    uint16_t max_tx_octets;
+
+	    /* Max Tx Time */
+	    uint16_t max_tx_time;
+
+	    /* Max Rx payload octet */
+	    uint16_t max_rx_octets;
+
+	    /* Max Rx Time */
+	    uint16_t max_rx_time;
+	} data_len_chg;
     };
 };
 
